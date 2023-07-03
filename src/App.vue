fd7982a8326322abc80f7fe267c4d0887832bf0f
Niki Dekany <nikolett.dekany@manyone.com>
2023-07-03 15:40:13 +0200

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import FieldPlugin from './components/FieldPlugin.vue'
+import FieldPluginExample from './components/FieldPluginExample/index.vue'
+import FieldPluginProvider from './components/FieldPluginProvider.vue'
+</script>
+
+<template>
+  <FieldPluginProvider>
+    <template v-slot:loading><span>Loading...</span></template>
+    <template v-slot:error><span>Error</span></template>
+
+    <FieldPluginExample />
+  </FieldPluginProvider>
+</template>
