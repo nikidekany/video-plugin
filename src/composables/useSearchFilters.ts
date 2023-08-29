18a7bb707ff766b94e5154ecbf92f0e301b4023a
Niki Dekany <nikolett.dekany@manyone.com>
2023-08-29 16:09:18 +0200

    comments

--- src/composables/useSearchFilters.ts
+++ src/composables/useSearchFilters.ts
@@ -1,14 +1,14 @@
 import { Ref, ref, watch } from 'vue'
 
 export function useSearchFilter<T extends { name: string }>(list: T[]) {
-  const filteredList: Ref<T[]> = ref(list) as Ref<T[]> // Perform the type assertion on the ref
+  const filteredList: Ref<T[]> = ref(list) as Ref<T[]>
   const filterText: Ref<string> = ref('')
 
   watch(filterText, (newVal) => {
     const filtered = list.filter((item) =>
       item.name.toLowerCase().includes(newVal.toLowerCase()),
     )
-    filteredList.value = filtered as T[] // Perform the type assertion on the value
+    filteredList.value = filtered as T[]
   })
 
   return {
